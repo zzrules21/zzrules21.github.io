87c88f8a6a18692ef599f14cbcb72f2a0a5c1590
Zach <zjbzscores@yahoo.com>
2020-11-30 04:35:08 -0500

    Add files via upload

--- bingo/tables/bl2.js
+++ bingo/tables/bl2.js
@@ -12,21 +12,21 @@ bingoList[2] = [
     { name:"Pay the toll in Bloodshot Ramparts", types: ["ramparts"] },
     { name:"Find Solitare's Camp in the Caustic Caverns", types: ["caustic"] },
     { name:"Recount the Story of Helena Pierce", types: ["shelf"] },
-    { name:"Complete 'Do No Harm'", types: ["shelf"] }
+    { name:"Complete 'Do No Harm'", types: ["sanctuary", "mystery"] }
 ];
 bingoList[3] = [
     { name:"Clear the Caustic Cavern mines of Creepers", types: ["caustic"] },
     { name:"Buy a Shield from the Old Crimson Raider Hideout", types: ["shelf"] },
     { name:"Complete 'In Memoriam'", types: ["divide"] },
     { name:"Decorate Ellie's Garage with hood ornaments", types: ["dust"] },
-    { name:"Start the Clan War between the Zafords and Hodunks", types: ["dust", "highlands"] },
+    { name:"Start the Clan War between the Zafords and Hodunks", types: ["dust", "highlands", "war"] },
     { name:"Complete 'Cult Following: Eternal Flame'", types: ["cult", "frostburn"] },
     { name:"Kill all 5 members of the Splinter Group", types: ["bloodshot"] },
     { name:"Complete 'No Hard Feelings'", types: ["tundra"] }
 ];
 bingoList[4] = [
     { name:"Complete 'The Name Game'", types: ["shelf", "divide"] },
-    { name:"Kill Doc Mercy", types: ["valley"] },
+    { name:"Kill Doc Mercy", types: ["valley", "mystery"] },
     { name:"Restore power to the Happy Pig Motel", types: ["valley"] },
     { name:"Complete 'To Close for Missiles'", types: ["dust"] },
     { name:"Complete 'Mine, All Mine'", types: ["tundra"] },
@@ -36,35 +36,35 @@ bingoList[4] = [
     { name:"Collect arms from around the Highlands'", types: ["highlands"] },
     { name:"Complete 'Swallowed Whole''", types: ["fridge"] },
     { name:"Uncover the history of Crystalisks'", types: ["caustic"] },
-    { name:"Deliver medicine to the residents of Overlook'", types: ["highlands"] }
+    { name:"Deliver medicine to the residents of Overlook'", types: ["highlands", "karima"] }
 ];
 bingoList[5] = [
     { name:"Complete 'Out of Body Experience'", types: ["ramparts"] },
     { name:"Complete 'Mighty Morphin''", types: ["tundra", "caustic"] },
     { name:"Complete 'Bad Hair Day'", types: ["shelf", "divide"] },
     { name:"Kill the 4 assassins in Southpaw Steam and Power", types: ["southpaw"] },
-    { name:"Kill the False Idol Scorch", types: ["frostburn"] },
+    { name:"Kill the False Idol Scorch", types: ["frostburn", "cult"] },
     { name:"Recover Mordecai's loot from the Dust", types: ["dust"] },
     { name:"Un-brainwash Laney White", types: ["fridge"] }
 ];
 bingoList[6] = [
     { name:"Jump on all 5 couches in Bloodshot Stronghold", types: ["bloodshot"] },
     { name:"Deliver packages to the residents of Split Skull Bay", types: ["valley"] },
-    { name:"Complete 'You Are Cordially Invited: Party Prep'", types: ["tundra"] },
+    { name:"Complete 'You Are Cordially Invited: Party Prep'", types: ["tundra", "tea"] },
     { name:"Kill Ol' Slappy", types: ["highlands"] },
     { name:"Escort the minecart through the Caustic Caverns", types: ["caustic"] },
     { name:"Attend Claptrap's Birthday Bash", types: ["sanctuary"] },
-    { name:"Crash the race at the Hodunk Speedway", types: ["dust"] },
-    { name:"Build a protective shield for Overlook", types: ["highlands"] },
+    { name:"Crash the race at the Hodunk Speedway", types: ["dust", "war"] },
+    { name:"Build a protective shield for Overlook", types: ["highlands", "karima"] },
     { name:"Collect Tannis' journals around the Highlands", types: ["highlands"] },
     { name:"Complete Doctor's Orders", types: ["wep"] }
 ];
 bingoList[7] = [
-    { name:"Invite Fleshstick to Tina's tea party", types: ["tundra"] },
+    { name:"Invite Fleshstick to Tina's tea party", types: ["tundra", "tea"] },
     { name:"Rob the Hyperion train in Ripoff Station", types: ["tundra"] },
     { name:"Complete 'Ice Man Cometh'", types: ["valley, divide"] },
     { name:"Avenge Taggert by killing Henry", types: ["highlands"] },
-    { name:"Reach the Hodunk's dead drop in the Holy Spirits", types: ["highlands"] },
+    { name:"Reach the Hodunk's dead drop in the Holy Spirits", types: ["highlands", "war"] },
 ];
 bingoList[8] = [
     { name:"Complete 'Note for Self-Person'", types: ["fridge"] },
@@ -74,24 +74,24 @@ bingoList[8] = [
 ];
 bingoList[9] = [
     { name:"Kill Midgemong and his midget rider", types: ["shelf"] },
-    { name:"Complete 'Medical Mystery: X-Com-municate'", types: ["valleyc"] },
-    { name:"Escort Matchstick to Flynt's ship", types: ["shelf"] },
+    { name:"Complete 'Medical Mystery: X-Com-municate'", types: ["valley", "mystery"] },
+    { name:"Escort Matchstick to Flynt's ship", types: ["shelf", "cult"] },
     { name:"Complete the second round of the Bandit Slaughter", types: ["finks", "slaughter"] },
-    { name:"Fully test Overlook's new shield", types: ["highlands"] },
-    { name:"Defend Tina's tea party from hostile dinner guests", types: ["tundra"] },
-    { name:"Kill the Bagman in the Zaford cash stash", types: ["highlands"] }
+    { name:"Fully test Overlook's new shield", types: ["highlands", "karima"] },
+    { name:"Defend Tina's tea party from hostile dinner guests", types: ["tundra", "tea"] },
+    { name:"Kill the Bagman in the Zaford cash stash", types: ["highlands", "war"] }
 ];
 bingoList[10] = [
     { name:"Complete 'Rakkaholics Anonymous'", types: ["dust"] },
-    { name:"Destroy the Hodunk's trailers", types: ["dust"] },
+    { name:"Destroy the Hodunk's trailers", types: ["dust", "war"] },
     { name:"Complete 'Poetic License'", types: ["1kcuts"] },
-    { name:"Complete 'Cult Following: The Enkindling'", types: ["frostburn"] }
+    { name:"Complete 'Cult Following: The Enkindling'", types: ["frostburn", "cult"] }
 ];
 bingoList[11] = [
-    { name:"Crash the Zaford's Wake", types: ["highlands"] },
+    { name:"Crash the Zaford's Wake", types: ["highlands", "war"] },
     { name:"Complete 'The Once and Future Slab'", types: ["1kcuts", "story"] },
     { name:"Complete the third round of the Bandit Slaughter", types: ["finks", "slaughter"] },
-    { name:"Complete 'Clan War: Zafords vs Hodunks'", types: ["dust"] }
+    { name:"Complete 'Clan War: Zafords vs Hodunks'", types: ["dust", "war"] }
 ];
 bingoList[12] = [
     { name:"Rescue the animals from the Wildlife Exploitation Preserve", types: ["wep"] },
@@ -102,18 +102,18 @@ bingoList[12] = [
     { name:"Blow up the Eridium train in Lynchwood", types: ["lynchwood"] },
 ];
 bingoList[13] = [
-    { name:"Complete 'Animal Rescue: Medicine'", types: ["lynchwood"] },
+    { name:"Complete 'Animal Rescue: Medicine'", types: ["lynchwood", "rescue"] },
     { name:"Complete 'Home Movies'", types: ["opportunity"] },
     { name:"Defend Slab Tower from the Hyperion onslaught", types: ["1kcuts"] }
 ];
 bingoList[14] = [
     { name:"Acquire The Bane", types: ["dust", "lynchwood"] },
     { name:"Complete 'Breaking the Bank'", types: ["lynchwood"] },
-    { name:"Complete 'Animal Rescue: Food'", types: ["lynchwood"] }
+    { name:"Complete 'Animal Rescue: Food'", types: ["lynchwood", "rescue"], }
 ];
 bingoList[15] = [
     { name:"Complete the fourth round of the Bandit Slaughter", types: ["finks", "slaughter"] },
-    { name:"Complete 'Animal Rescue: Shelter'", types: ["lynchwood"] },
+    { name:"Complete 'Animal Rescue: Shelter'", types: ["lynchwood", "rescue"] },
     { name:"Kill Bunker", types: ["1kcuts", "story"] }
 ];
 bingoList[16] = [
@@ -129,7 +129,7 @@ bingoList[17] = [
     { name:"Call the Hyperion Suicide Prevention Hotline", types: ["blight"] }
 ];
 bingoList[18] = [
-    { name:"Find Clothes for Mal", types: ["blight"] },
+    { name:"Find Clothes for Mal", types: ["blight", "mal"] },
     { name:"Check on Jack's Grandmother", types: ["blight"] }
 ];
 bingoList[19] = [
@@ -143,13 +143,13 @@ bingoList[20] = [
 ];
 bingoList[21] = [
     { name:"Kill Bonehead 2.0", types: ["badlands"] },
-    { name:"Collect Limbs for Mal", types: ["blight"] },
+    { name:"Collect Limbs for Mal", types: ["blight", "mal"] },
     { name:"Complete the second round of the Hyperion Slaughter", types: ["chasm", "slaughter"] },
-    { name:"Kill Mal", types: ["blight"] },
+    { name:"Kill Mal", types: ["blight", "mal"] },
     { name:"Kill Hunter hellquest", types: ["boneyard"] }
 ];
 bingoList[22] = [
-    { name:"Kill Dukino's Mom", types: ["lynchwood"] },
+    { name:"Kill Dukino's Mom", types: ["lynchwood", "rescue"] },
     { name:"Complete 'Uncle Teddy'", types: ["badlands"] },
     { name:"Complete 'Monster Mash (Part 3)", types: ["boneyard", "frostburn", "mash"] }
 ];
